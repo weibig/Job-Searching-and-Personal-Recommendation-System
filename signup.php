c52301a4d7e776b145794c4c170ea664d42b7d3b
System Administrator <root@eva-2.local>
2020-01-16 03:52:26 +0800

    revise

--- signup.php
+++ signup.php
@@ -34,8 +34,8 @@
   $link = mysqli_connect("localhost","root",
                             "","final_project")
           or die("無法開啟MySQL資料庫連接!<br/>");
-  //送出UTF8編碼的MySQL指令
-  mysqli_query($link, 'SET NAMES utf8'); 
+ //送出UTF8編碼的MySQL指令
+  mysqli_query($link, 'SET NAMES utf8');
   $_SESSION["login_session"] = false;
 
 
@@ -51,6 +51,7 @@
 
 
    $signName = "";  $gender = ""; $ability1 = "";  $ability2 = ""; $ability3 = ""; $ability4 = "";  $ability5 = ""; $ability6 = "";  $ability7 = ""; $ability8 = "";  $ability9 = ""; $ability10 = "";  $signEmail = "";  $signTel = "";  $signAge = "";  $signPwd1 = ""; $signPwd2 = ""; 
+  
    
 
   // 取得signup
@@ -93,16 +94,17 @@
   if ( isset($_POST["signPwd1"]) )
      $signPwd1 = $_POST["signPwd1"];
   if ( isset($_POST["signPwd2"]) )
-     $signPwd1 = $_POST["signPwd2"];
+     $signPwd2 = $_POST["signPwd2"];
 
   // 檢查是否輸入使用者名稱和密碼
   if ($logEmail != "" && $logPwd != "") {
     // echo 123;
      
+     $errormsgSign = ""; $errormsgLog = "";
      
      // 建立SQL指令字串
-     $sqlLog = "SELECT * FROM user WHERE password='";
-     $sqlLog.= $logPwd."' AND email='".$logEmail."'";
+     $sqlLog = "SELECT * FROM user WHERE Password='";
+     $sqlLog.= $logPwd."' AND Email='".$logEmail."'";
      // echo $logEmail;
      // echo $logPwd;
      // 執行SQL查詢
@@ -113,34 +115,55 @@
         // 成功登入, 指定Session變數
         $_SESSION["login_session"] = true;
         header("Location: index.php");
-     } else {  // 登入失敗
-        // echo "<center><font color='red'>";
-        // echo "使用者名稱或密碼錯誤!<br/>";
-        // echo "</font>";
+     } else {  
+        $errormsgLog = "Log in error!!";
         $_SESSION["login_session"] = false;
      }
      
   }
-  else if($signName != "" && $gender != "" &&  $signEmail != "" &&  $signTel != "" &&  $signAge != "" &&  $signPwd1 != "" && $signPwd2 != "" && ($signPwd1 === $signPwd2))
+  else if($signName != "" && $gender != "" &&  $signEmail != "" &&  $signTel != "" &&  $signAge != "" &&  $signPwd1 != "" && $signPwd2 != "" && ($signPwd1 == $signPwd2))
   {
 
+
+    // 執行UID查詢
+     $id = mysqli_query($link, "SELECT COUNT(*) FROM user ");
+     $id = mysqli_fetch_row($id)[0];
       
-    $sqlSign = "INSERT INTO user( Name, Gender, Email, Telephone, Age, Password) VALUES (".$signName.",".$gender.",".$signEmail.",".$signTel.",".$signAge.",".$signPwd1.")";
+     $sqlSign = "INSERT INTO user ( UID, Name, Gender, Email, Telephone, Age, Password) VALUES (".$id.",'".$signName."','".$gender."','".$signEmail."','".$signTel."','".$signAge."','".$signPwd1."')";
+    
+   $i = 1;
+   while($i < 11)
+    {
+      $v = 'ability'.($i++);
+      if($$v == "on")
+      {
+           $sqlAbility= "INSERT INTO User_has_ability( UName, AID) VALUES ('".$signName."','".$i."')";
+      }
+    }
 
 
-    $sqlAbility = "INSERT INTO User has ability( UID, AID) VALUES (".$ability1 .",". $ability2 .",". $ability3.",". $ability4 .",". $ability5 .",". $ability6 .",".  $ability7 .",". $ability8 .",".  $ability9 .",". $ability10 .")";
 
-    if ($link->multi_query($sqlSign.$sqlAbility) === TRUE)
-      echo "註冊成功";
-  }
+    //if (mysqli::multi_query($link, $sqlSign.";".$sqlAbility) === TRUE) //$sqlAbility
+    if(!mysqli_query($link, $sqlSign))
+    {
+      $errormsgSign = "Sign up error!!";
+      printf("Error message: %s\n", mysqli_error($link));
+    }
 
-  if($signPwd1 !== $signPwd2)
+    if(!mysqli_query($link, $sqlAbility))
+    {
+      printf("Error message: %s\n", mysqli_error($link));
+    }
+
+  if($signPwd1 != $signPwd2)
   {
       echo "密碼輸入錯誤";
   }
+}
 
+  // echo $sqlSign.$sqlAbility;
   mysqli_close($link);  // 關閉資料庫連接  
-  ?>
+?>
 
   <div class ="site-wrap">
 
@@ -310,6 +333,12 @@
                 <div class="col-md-12">
                   <input type="submit" value="Sign Up" class="btn btn-primary btn-md text-white">
                 </div>
+                <div class="col-md-12">
+                  <?php
+                  if(errormsgSign != "")
+                      echo errormsgSign;
+                  ?>
+                </div>
               </div>
 
   
@@ -346,6 +375,13 @@
                 <div class="col-md-12">
                   <input type="submit" value="Log In" class="btn btn-primary btn-md text-white">
                 </div>
+                <div class="col-md-12">
+                   <?php
+                  if(errormsgLog != "")
+                      echo errormsgLog;
+                  ?>
+                </div>
+               
               </div>
 
   
@@ -383,3 +419,4 @@
   
   </body>
 </html>
+
